0ca881ad46e009cc1039437e9c6f72a093f0d608
Lucky Eni <realchartwitluckyenison12>
2026-03-08 17:10:23 +0100

    project done

--- server.js
+++ server.js
@@ -1,44 +1,36 @@
 /* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
+ * CSE Motors Application Server
+ * ******************************************/
 
-/* ***********************
- * Require Statements
- *************************/
 const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
+const path = require("path")
+
 const app = express()
-const static = require("./routes/static")
 
-/* ***********************
- * View Engine and Templates
- *************************/
+/* ******************************************
+ * View Engine
+ * ******************************************/
 app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout")
+app.set("views", path.join(__dirname, "views"))
 
-/* ***********************
- * Routes
- *************************/
-app.use(static)
+/* ******************************************
+ * Static Files
+ * ******************************************/
+app.use(express.static(path.join(__dirname, "public")))
 
-// Index route for the home page
-app.get("/", function(req, res){
-  res.render("index", { title: "Home" })
+/* ******************************************
+ * Index Route
+ * ******************************************/
+app.get("/", (req, res) => {
+  res.render("index", { title: "CSE Motors | Home" })
 })
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+/* ******************************************
+ * Server Information
+ * ******************************************/
+const PORT = 5500
+const HOST = "localhost"
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
-app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+app.listen(PORT, () => {
+  console.log(`Server running at http://${HOST}:${PORT}`)
 })
